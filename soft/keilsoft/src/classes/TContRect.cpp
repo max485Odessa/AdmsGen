459d0acc190df10e639c69edfd1f2e17206acb25
max485Odessa <maxaldoshin@gmail.com>
2023-10-12 21:29:59 +0300

    управление таймерами

--- soft/keilsoft/src/classes/TContRect.cpp
+++ soft/keilsoft/src/classes/TContRect.cpp
@@ -5,18 +5,39 @@
 TCONTRECT::TCONTRECT (S_GPIOPIN *pnout, S_GPIOPIN *pnin, ESYSTIM tt, uint32_t freq_hz, uint8_t cmagcnt) : c_magnet_cnt (cmagcnt), IEXTINT_ISR(pnin, EGPINTMOD_RISING_FALLING), ITIM_ISR (tt)
 {
 	timer_init (0xFFFFFFFF, freq_hz);
+	enable_timer_oc (EPWMCHNL_PWM1, true);
+	enable_timer_oc (EPWMCHNL_PWM2, true);
+	
 	c_pin_out = pnout;
-	rpmcontr_sw = ERPMSW_NONE;
 	timer_timeout_ext_isr.set (0);
-	f_phase_value_new_isr = false;
-	f_phase_value_new_sys = false;
 	f_speed_control_enabled = false;
+
+	enable_timer_isr (false);
+	rectifier_enabled (false);
+
+	local_rpm_tick_counter = 0;
+	result_rpm_tick_counter = 0;
+	
+	rpm_handler = create_timer_ev (this);
+	timer_start_periodic (rpm_handler, 1000);
+}
+
+
+
+void TCONTRECT::periodic_cb (SYSBIOS::HPERIOD h)
+{
+	if (rpm_handler == h)
+		{
+		result_rpm_tick_counter = local_rpm_tick_counter;
+		local_rpm_tick_counter = 0;
+		}
 }
 
 
 
 float TCONTRECT::get_rpm ()
 {
+	return result_rpm_tick_counter;
 }
 
 
@@ -31,12 +52,43 @@ void TCONTRECT::control_rectifier (float strt_angl, float stp_angl)
 
 void TCONTRECT::rectifier_enabled (bool val)
 {
+	oc_control_enabled (val);
+	f_rectifier_system_enabled = val;
+}
+
+
+
+void TCONTRECT::gpio_rectifier_enable (bool val)
+{
+	_pin_output (c_pin_out, val);
+}
+
+
+
+//bool f_oc_interupt_enabled
+void TCONTRECT::oc_control_enabled (bool val)
+{
+	rpmcontr_sw = ERPMSW_NONE;
+	f_phase_value_new_isr = false;
+	f_phase_value_new_sys = false;
+	f_oc_interupt_enabled = false;
+	f_phase_control_active = false;
+	timer_timeout_ext_isr.set (0);
+
+	enable_timer_isr (val);
+
+	gpio_rectifier_enable (false);
+	f_oc_interupt_enabled = val;
+
 }
 
 
 
 void TCONTRECT::Task ()
 {
+	
+	if (!f_rectifier_system_enabled) return;
+	
 	if (!timer_timeout_ext_isr.get ())
 		{
 		f_phase_control_active = false;
@@ -68,7 +120,12 @@ void TCONTRECT::Task ()
 				tick_phase_rectifier_sys_on = program_rect_angle_on * val_quant;
 				tick_phase_rectifier_sys_off = program_rect_angle_off * val_quant;
 				f_phase_value_new_sys = true;
-				}			
+				}	
+			if (f_phase_value_new_isr && !f_oc_interupt_enabled) oc_control_enabled (true);
+			}
+		else
+			{
+			if (f_oc_interupt_enabled) oc_control_enabled (false);
 			}
 		}
 }
@@ -77,12 +134,40 @@ void TCONTRECT::Task ()
 
 void TCONTRECT::tim_comp_cb_isr (ESYSTIM t, EPWMCHNL ch)
 {
+	if (t == ESYSTIM_TIM2)
+		{
+		if (f_oc_interupt_enabled)
+			{
+			switch (ch)
+				{
+				case EPWMCHNL_PWM1:
+					{
+					gpio_rectifier_enable (true);
+					break;
+					}
+				case EPWMCHNL_PWM2:
+					{
+					gpio_rectifier_enable (false);
+					break;
+					}
+				}
+			}
+		else
+			{
+			gpio_rectifier_enable (false);
+			}
+		}
 }
 
 
 
 void TCONTRECT::update_phase_points (uint32_t tcnreg)
 {
+	if (f_oc_interupt_enabled)
+		{
+		set_timer_oc_value (EPWMCHNL_PWM1, tcnreg + tick_phase_rectifier_isr_on);
+		set_timer_oc_value (EPWMCHNL_PWM2, tcnreg + tick_phase_rectifier_isr_off);
+		}
 }
 
 
@@ -129,6 +214,7 @@ void TCONTRECT::isr_gpio_cb_int (uint8_t isr_n, bool pinstate)
 				if (f_phase_control_active && f_phase_value_new_isr) update_phase_points (cur_meas_tick);
 					
 				rpmcontr_sw = ERPMSW_FRONTE_0_360_ANGLE;
+				local_rpm_tick_counter++;
 				}
 			else
 				{
@@ -142,7 +228,6 @@ void TCONTRECT::isr_gpio_cb_int (uint8_t isr_n, bool pinstate)
 			break;
 			}
 		}
-		
 	if (f_error)
 		{
 		rpmcontr_sw = ERPMSW_FRONTE_0_360_ANGLE;
--- soft/keilsoft/src/classes/TContRect.h
+++ soft/keilsoft/src/classes/TContRect.h
@@ -17,8 +17,12 @@ enum ERPMSW {ERPMSW_NONE = 0, ERPMSW_FRONTE_0_360_ANGLE, ERPMSW_FALL_180_ANGLE, 
 
 
 
-class TCONTRECT: public ITIM_ISR, public IEXTINT_ISR, public TFFC {
-
+class TCONTRECT: public ITIM_ISR, public IEXTINT_ISR, public TFFC, public SYSBIOS::Periodic {
+		uint32_t local_rpm_tick_counter;
+		uint32_t result_rpm_tick_counter;
+		virtual void periodic_cb (SYSBIOS::HPERIOD h) override;
+		SYSBIOS::HPERIOD rpm_handler;
+	
 		uint32_t tick_delta_360_Angle;
 		uint32_t tick_0_angle;					// rise
 		uint32_t tick_180_angle;				// rise
@@ -36,6 +40,9 @@ class TCONTRECT: public ITIM_ISR, public IEXTINT_ISR, public TFFC {
 		
 		bool f_phase_control_active;
 	
+		bool f_oc_interupt_enabled;
+		void oc_control_enabled (bool val);
+	
 		SYSBIOS::Timer timer_timeout_ext_isr;
 	
 		float program_rect_angle_on;
@@ -45,6 +52,8 @@ class TCONTRECT: public ITIM_ISR, public IEXTINT_ISR, public TFFC {
 		void update_phase_points (uint32_t tcnreg);
 	
 		S_GPIOPIN *c_pin_out;
+		
+		void gpio_rectifier_enable (bool val);
 	
 		virtual void tim_comp_cb_isr (ESYSTIM t, EPWMCHNL ch) override;
 		virtual void isr_gpio_cb_int (uint8_t isr_n, bool pinstate) override;
@@ -53,7 +62,7 @@ class TCONTRECT: public ITIM_ISR, public IEXTINT_ISR, public TFFC {
 	
 	protected:
 	
-		bool f_rect_enabled;
+		bool f_rectifier_system_enabled;
 	
 	public:
 		TCONTRECT (S_GPIOPIN *pnout, S_GPIOPIN *pnin, ESYSTIM tt, uint32_t freq_hz, uint8_t cmagcnt);
